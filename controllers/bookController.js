2a22d02249ca8b4391760fcf237ab9e2a6658a49
Abdul Khalid <mezmerizingbeats2004@gmail.com>
2024-03-16 18:19:56 +0530

    feat: implement book deleting feature

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -151,12 +151,22 @@ exports.book_create_post = [
 
 // Display book delete form on GET.
 exports.book_delete_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Book delete GET");
+  const [book, bookInstances] = await Promise.all([
+    Book.findById(req.params.id).populate("author").populate("genre").exec(),
+    BookInstance.find({ book: req.params.id }).exec(),
+  ]);
+
+  res.render("book_delete", {
+    title: book.title,
+    book: book,
+    book_instances: bookInstances,
+  });
 });
 
 // Handle book delete on POST.
 exports.book_delete_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Book delete POST");
+  await Book.findByIdAndDelete(req.body.bookid);
+  res.redirect("/catalog/books");
 });
 
 // Display book update form on GET.
