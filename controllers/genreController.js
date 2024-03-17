0b67fc59b1a9824edaedf5d90449df5d2af3f06f
Abdul Khalid <mezmerizingbeats2004@gmail.com>
2024-03-17 21:55:47 +0530

    feat: implement genre deleting feature

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -84,12 +84,22 @@ exports.genre_create_post = [
 
 // Display Genre delete form on GET.
 exports.genre_delete_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Genre delete GET");
+  const [genre, booksInGenre] = await Promise.all([
+    Genre.findById(req.params.id).exec(),
+    Book.find({ genre: req.params.id }, "title").exec(),
+  ]);
+
+  res.render("genre_delete", {
+    title: "Genre Detail",
+    genre: genre,
+    genre_books: booksInGenre,
+  });
 });
 
 // Handle Genre delete on POST.
 exports.genre_delete_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Genre delete POST");
+  await Genre.findByIdAndDelete(req.body.genreid);
+  res.redirect("/catalog/genres");
 });
 
 // Display Genre update form on GET.
